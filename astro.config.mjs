27b5b73fc94f99c7bb5da2c44420a3baf2aba93d
jpark6 <qkstjr09@gmail.com>
2025-05-31 10:50:25 +0900

    Feat: Init Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jpark6.github.io',
+  base: 'astro-blog',
 	integrations: [
 		starlight({
 			title: 'My Docs',
