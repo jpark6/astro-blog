b7e9687a71b5c1aaecfc2a126592cf4cc6f63fca
jpark6 <qkstjr09@gmail.com>
2025-05-31 11:28:37 +0900

    Feat: change base '/'

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jpark6.github.io',
+  base: '/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
